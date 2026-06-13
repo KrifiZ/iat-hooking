e6ce7ae4e5d664e20e03c5c1eedce92232d2ec94
Michał <michalmuller@onet.eu>
2026-06-13 16:30:00 +0200

    Refactor with helper functions, use MSDN byte representation for structures

--- main.cpp
+++ main.cpp
@@ -15,6 +15,30 @@ bool isEmpty(BYTE* importVal, size_t size) {
 }
 #define IS_STRUCT_EMPTY(ptr) isEmpty((BYTE*)ptr, sizeof *ptr )
 
+void printSystemVersion(PIMAGE_NT_HEADERS newTechnologyHeaders) {
+	if (newTechnologyHeaders->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
+		printf("Program for 32-bit\n");
+	}
+	else {
+		printf("Program for 64-bit\n");
+	}
+}
+
+void printDllName(PIMAGE_IMPORT_DESCRIPTOR importDirectoryTable, PIMAGE_DOS_HEADER pdosHeader) {
+	const char* dllName = (const char*)((BYTE*)pdosHeader + importDirectoryTable->Name);
+	printf("-----dll name-----\n");
+	printf("%s\n", dllName);
+}
+
+void printDllFunctionsName(PIMAGE_THUNK_DATA32 lookupTable, PIMAGE_DOS_HEADER pdosHeader) {
+	printf("-----functions-----\n");
+	while (!IS_STRUCT_EMPTY(lookupTable)) {
+		PIMAGE_IMPORT_BY_NAME functionName = (PIMAGE_IMPORT_BY_NAME)((BYTE*)pdosHeader + lookupTable->u1.Function);
+		printf("%s \n", functionName->Name);
+		lookupTable += 1;
+	}
+}
+
 int main() {
 	HMODULE appHandle = GetModuleHandle(NULL);
 	printf("Application handle: %p\n", appHandle);
@@ -27,35 +51,22 @@ int main() {
 
 	printf("DOS header found\n");
 
-	// Parse NT headers
+	// Parse NT headers using byte representation from MSDN
 	PIMAGE_NT_HEADERS newTechnologyHeaders = (PIMAGE_NT_HEADERS)((BYTE*)pdosHeader + pdosHeader->e_lfanew);
 
-	// Check magic field for architecture
-	if (newTechnologyHeaders->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
-		printf("Program for 32-bit\n");
-	}
-	else {
-		printf("Program for 64-bit\n");
-	}
+	printSystemVersion(newTechnologyHeaders);
 
 	// Get Import Directory Table address from DataDirectory
 	DWORD importRVA = newTechnologyHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
 	PIMAGE_IMPORT_DESCRIPTOR importDirectoryTable = (PIMAGE_IMPORT_DESCRIPTOR)((BYTE*)pdosHeader + importRVA);
 
-	printf("Import Directory Table found\n");
-
 	// Iterate through imported DLLs
 	while (!IS_STRUCT_EMPTY(importDirectoryTable)) {
-		const char* dllName = (const char*)((BYTE*)pdosHeader + importDirectoryTable->Name);
-		printf("DLL: %s\n", dllName);
+		printDllName(importDirectoryTable, pdosHeader);
 
 		// Iterate through thunk table for function names
 		PIMAGE_THUNK_DATA32 lookupTable = (PIMAGE_THUNK_DATA32)((BYTE*)pdosHeader + importDirectoryTable->OriginalFirstThunk);
-		while (!IS_STRUCT_EMPTY(lookupTable)) {
-			PIMAGE_IMPORT_BY_NAME functionName = (PIMAGE_IMPORT_BY_NAME)((BYTE*)pdosHeader + lookupTable->u1.Function);
-			printf("  Function: %s\n", functionName->Name);
-			lookupTable += 1;
-		}
+		printDllFunctionsName(lookupTable, pdosHeader);
 
 		importDirectoryTable = importDirectoryTable + 1;
 	}
