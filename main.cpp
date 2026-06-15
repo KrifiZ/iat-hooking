26aeb4d29aeece09c64030c9d3f65df3c91bb724
Michał <michalmuller@onet.eu>
2026-06-15 18:00:00 +0200

    Assign proper variable names and implement working PE header printing

--- main.cpp
+++ main.cpp
@@ -11,9 +11,18 @@ bool isEmpty(BYTE* importVal, size_t size) {
 	if (sum == size) {
 		return 1;
 	}
+
 	return 0;
+
 }
 #define IS_STRUCT_EMPTY(ptr) isEmpty((BYTE*)ptr, sizeof *ptr )
+
+void printDllName(PIMAGE_IMPORT_DESCRIPTOR importDirectoryTable, PIMAGE_DOS_HEADER pdosHeader) {
+	DWORD nameRVA = importDirectoryTable->Name;
+	const char* dllName = (const char*)((BYTE*)pdosHeader + importDirectoryTable->Name);
+	printf("-----dll name-----\n");
+	printf("%s\n", dllName);
+}
 
 void printSystemVersion(PIMAGE_NT_HEADERS newTechnologyHeaders) {
 	if (newTechnologyHeaders->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
@@ -22,12 +31,6 @@ void printSystemVersion(PIMAGE_NT_HEADERS newTechnologyHeaders) {
 	else {
 		printf("Program for 64-bit\n");
 	}
-}
-
-void printDllName(PIMAGE_IMPORT_DESCRIPTOR importDirectoryTable, PIMAGE_DOS_HEADER pdosHeader) {
-	const char* dllName = (const char*)((BYTE*)pdosHeader + importDirectoryTable->Name);
-	printf("-----dll name-----\n");
-	printf("%s\n", dllName);
 }
 
 void printDllFunctionsName(PIMAGE_THUNK_DATA32 lookupTable, PIMAGE_DOS_HEADER pdosHeader) {
@@ -48,48 +51,35 @@ int WINAPI fakeBox(HWND, LPCSTR, LPCSTR, UINT) {
 
 int main() {
 	HMODULE appHandle = GetModuleHandle(NULL);
-	printf("Application handle: %p\n", appHandle);
-
+	/* Finding all structures from https://learn.microsoft.com/en-us/windows/win32/debug/pe-format
+		where there are no simple structures described in winnt.h, you have to guess by the byte sizes
+		of structures. Variable names are not perfectly matched to how PE viewers look.
+		Lots of own work and AI assistance.
+	*/
 	PIMAGE_DOS_HEADER pdosHeader = (PIMAGE_DOS_HEADER)appHandle;
 	if (pdosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
-		printf("Invalid DOS signature\n");
 		return 1;
 	}
 
-	printf("DOS header found\n");
-
-	// Parse NT headers using byte representation from MSDN
 	PIMAGE_NT_HEADERS newTechnologyHeaders = (PIMAGE_NT_HEADERS)((BYTE*)pdosHeader + pdosHeader->e_lfanew);
 
 	printSystemVersion(newTechnologyHeaders);
 
-	// Get Import Directory Table address from DataDirectory
 	DWORD importRVA = newTechnologyHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+
 	PIMAGE_IMPORT_DESCRIPTOR importDirectoryTable = (PIMAGE_IMPORT_DESCRIPTOR)((BYTE*)pdosHeader + importRVA);
 
-	// Search for USER32.dll and attempt to hook MessageBoxA
 	while (!IS_STRUCT_EMPTY(importDirectoryTable)) {
+		PIMAGE_THUNK_DATA32 lookupTable = (PIMAGE_THUNK_DATA32)((BYTE*)pdosHeader + importDirectoryTable->OriginalFirstThunk);
+		DWORD nameRVA = importDirectoryTable->Name;
+		int sum = 0;
 		const char* dllName = (const char*)((BYTE*)pdosHeader + importDirectoryTable->Name);
 		if (strcmp(dllName, "USER32.dll") == 0) {
 			printDllName(importDirectoryTable, pdosHeader);
-
-			// Iterate through thunk table for function names
-			PIMAGE_THUNK_DATA32 lookupTable = (PIMAGE_THUNK_DATA32)((BYTE*)pdosHeader + importDirectoryTable->OriginalFirstThunk);
-			int sum = 0;
-			while (!IS_STRUCT_EMPTY(lookupTable)) {
-				PIMAGE_IMPORT_BY_NAME functionName = (PIMAGE_IMPORT_BY_NAME)((BYTE*)pdosHeader + lookupTable->u1.Function);
-				printf("%s \n", functionName->Name);
-
-				if (strcmp(functionName->Name, "MessageBoxA") == 0) {
-					printf("Found MessageBoxA at index %d\n", sum);
-				}
-
-				lookupTable += 1;
-				sum++;
-			}
+			printDllFunctionsName(lookupTable, pdosHeader);
 		}
-
 		importDirectoryTable = importDirectoryTable + 1;
+
 	}
 
 	return 0;
